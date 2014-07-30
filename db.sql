d2a1eda48fe108c5847846db573152ed373a43ab
Ilya <ilya.chernyshov@gmail.com>
2014-07-30 17:07:52 +0400

    db

---
+++ db.sql
@@ -0,0 +1,32 @@
+-- --------------------------------------------------------
+-- Хост:                         127.0.0.1
+-- Версия сервера:               5.5.34-MariaDB - mariadb.org binary distribution
+-- ОС Сервера:                   Win32
+-- HeidiSQL Версия:              8.0.0.4396
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Дамп структуры базы данных urlshortener
+DROP DATABASE IF EXISTS `urlshortener`;
+CREATE DATABASE IF NOT EXISTS `urlshortener` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `urlshortener`;
+
+
+-- Дамп структуры для таблица urlshortener.url
+DROP TABLE IF EXISTS `url`;
+CREATE TABLE IF NOT EXISTS `url` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `original_url` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `user` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `createDatetime` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Экспортируемые данные не выделены.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
